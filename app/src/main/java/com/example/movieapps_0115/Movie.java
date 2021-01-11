54ef8737565391e8ac8c6209367111272dabdfed
rafisyahid21 <Muhammad.2107@students.amikom.ac.id>
2021-01-12 06:36:20 +0700

    MovieApps_0115

---
+++ app/src/main/java/com/example/movieapps_0115/Movie.java
@@ -0,0 +1,48 @@
+package com.example.movieapps_0115;
+
+public class Movie {
+    private String title;
+    private String year;
+    private String image;
+    private String overview;
+
+
+    public Movie(String title, String year, String image, String overview) {
+        this.title = title;
+        this.year = year;
+        this.image = image;
+        this.overview = overview;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+}
